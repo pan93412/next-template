ff58dcb13f67f2613a8ae8d4372034b13e219ebd
Yi-Jyun Pan <pan93412@gmail.com>
2021-05-21 01:16:33 +0800

    fix(announcement/[category]): check the type for details

--- pages/announcement/[category]/index.tsx
+++ pages/announcement/[category]/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ListAnnouncements } from "schweb-parser/dist";
 import {
   AnnouncementCategoryMetadata,
   isValidAnnouncementCategory,
@@ -9,12 +10,14 @@ import FieldsGroup from "../../../components/Field/FieldsGroup";
 import Field from "../../../components/Field/Field";
 import type { AnnouncementCategory } from "../../../common/AnnouncementCategory";
 import type { GetServerSideProps } from "next";
+import type { Announcement } from "schweb-parser/dist/types/announcements/types";
 
 interface Props {
   category: AnnouncementCategory;
+  data: Announcement[];
 }
 
-export default function AnnouncementOverviewPage({ category }: Props) {
+export default function AnnouncementOverviewPage({ data, category }: Props) {
   return (
     <BasePage id="announcement overview">
       <div className="p-4">
@@ -22,7 +25,7 @@ export default function AnnouncementOverviewPage({ category }: Props) {
           <Field
             title={`公告 / ${AnnouncementCategoryMetadata[category].type}`}
           >
-            <AnnouncementCards maxColumns={4} category={category} />
+            <AnnouncementCards data={data} maxColumns={4} category={category} />
           </Field>
         </FieldsGroup>
       </div>
@@ -30,21 +33,30 @@ export default function AnnouncementOverviewPage({ category }: Props) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context
+) => {
+  if (!context.query.category) throw new Error("unexpected: category is null");
   const { category } = context.query;
+  if (Array.isArray(category))
+    throw new Error("unexpected: category is an array");
 
-  if (!isValidAnnouncementCategory(category as string)) {
-    return {
-      redirect: {
-        destination: "/announcement/category/school",
-        permanent: true,
-      },
-    };
+  if (isValidAnnouncementCategory(category)) {
+    const data = await ListAnnouncements(category);
+    if (data) {
+      return {
+        props: {
+          category,
+          data: data.data,
+        },
+      };
+    }
   }
 
   return {
-    props: {
-      category,
+    redirect: {
+      destination: "/announcement/category/school",
+      permanent: true,
     },
   };
 };
