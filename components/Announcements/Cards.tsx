326e565844a47fc3209e315e57b15e40a715acd2
Yi-Jyun Pan <pan93412@gmail.com>
2021-05-21 01:15:51 +0800

    refactor(components/Announcements/AnnouncementBody): rewrite

--- components/Announcements/Cards.tsx
+++ components/Announcements/Cards.tsx
@@ -1,58 +1,51 @@
 import React from "react";
-import { ListAnnouncementsMeta } from "schweb-parser/dist";
-import useSWR from "swr";
 import { AnnouncementCategoryMetadata } from "../../common/AnnouncementCategory";
-import Card from "../Base/BaseCard";
+import BaseLargeCard from "../Base/BaseCardV2";
 import type { Announcement } from "schweb-parser/dist/types/announcements/types";
 import type { AnnouncementCategory } from "../../common/AnnouncementCategory";
 
-async function getAnnouncementsSWR(
-  input: RequestInfo,
-  init?: RequestInit
-): Promise<Announcement[] | never> {
-  const resp = await fetch(input, init);
-  if (!resp.ok)
-    throw new Error(`getAnnouncements: failed to fetch: ${input.toString()}`);
+// export async function getAnnouncementsSWR(
+//   input: RequestInfo,
+//   init?: RequestInit
+// ): Promise<Announcement[] | never> {
+//   const resp = await fetch(input, init);
+//   if (!resp.ok)
+//     throw new Error(`getAnnouncements: failed to fetch: ${input.toString()}`);
 
-  const json = (await resp.json()) as Promise<unknown>;
+//   const json = (await resp.json()) as Promise<unknown>;
 
-  if (!ListAnnouncementsMeta.checker(json)) {
-    throw new Error(
-      `getAnnouncements: data is invalid: ${JSON.stringify(json)}`
-    );
-  }
+//   if (!ListAnnouncementsMeta.checker(json)) {
+//     throw new Error(
+//       `getAnnouncements: data is invalid: ${JSON.stringify(json)}`
+//     );
+//   }
 
-  return json.data;
-}
+//   return json.data;
+// }
 
 export interface AnnouncementCardsProps {
+  data: Announcement[];
   category: AnnouncementCategory;
   maxColumns: number;
-  // maxCards?: number;
 }
 
-const endp = ListAnnouncementsMeta.endpoint;
-const useMySWR = (category: string) =>
-  useSWR(endp(category), getAnnouncementsSWR);
-
 export default function AnnouncementCards({
+  data,
   category,
   maxColumns,
 }: AnnouncementCardsProps) {
-  const data = useMySWR(category);
-
   return (
     <div
       className={`grid grid-flow-row grid-cols-${maxColumns} gap-3 auto-cols-fr`}
     >
-      {data.data?.map((announce) => (
-        <Card
+      {data.map(({ id, title }) => (
+        <BaseLargeCard
           subtitle={AnnouncementCategoryMetadata[category].type}
-          href={`/announcement/${category}/${announce.id}`}
-          key={`announcement-card-${announce.id}`}
+          href={`/announcement/${category}/${id}`}
+          key={`announcement-card-${id}`}
         >
-          {announce.title}
-        </Card>
+          {title}
+        </BaseLargeCard>
       ))}
     </div>
   );
