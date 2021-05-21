c885302e6ba797eb45bb8ae4e0e7ff8bea5bcb00
Yi-Jyun Pan <pan93412@gmail.com>
2021-05-21 08:38:07 +0800

    feat(components/Announcements/AnnouncementBody): use ReactHtmlParser

--- components/Announcements/AnnouncementBody.tsx
+++ components/Announcements/AnnouncementBody.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import React from "react";
+import ReactHtmlParser from "react-html-parser";
 import type { AnnouncementContent } from "schweb-parser/dist/types/announcements/types";
 
 export interface AnnouncementBodyProps {
@@ -8,7 +10,9 @@ export interface AnnouncementBodyProps {
 export default function AnnouncementBody({ data }: AnnouncementBodyProps) {
   return (
     <>
-      <div className="announcement content mb-6">{data.contentHTML}</div>
+      <div className="announcement content mb-6">
+        {ReactHtmlParser(data.contentHTML)}
+      </div>
       {data.attachments.length > 0 && (
         <div className="announcement attachments">
           <h2 className="text-xl font-bold mb-2">附件</h2>
