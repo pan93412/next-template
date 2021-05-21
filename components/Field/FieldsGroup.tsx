792e6f8958c441410e40e7fe8f387c62298ca3dd
Yi-Jyun Pan <pan93412@gmail.com>
2021-05-21 10:04:34 +0800

    feat(components/Field/FieldsGroup): overflow-scroll

--- components/Field/FieldsGroup.tsx
+++ components/Field/FieldsGroup.tsx
@@ -6,5 +6,9 @@ export interface FieldsGroupProps {
 }
 
 export default function FieldsGroup({ children }: FieldsGroupProps) {
-  return <div className="flex justify-between w-full">{children}</div>;
+  return (
+    <div className="flex justify-between w-full overflow-scroll">
+      {children}
+    </div>
+  );
 }
