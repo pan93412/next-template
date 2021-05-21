cd8f3ba913cc42a7eac260acfe80c858bdc7eaf0
Yi-Jyun Pan <pan93412@gmail.com>
2021-05-21 08:55:43 +0800

    feat(components/Navbar/Navbar): add back button

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import Link from "next/link";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/dist/client/router";
 // import SearchBar from "./SearchBar";
 
 export default function Navbar() {
+  const router = useRouter();
+
   return (
     <div className="px-8 py-4 grid grid-col-1 md:grid-col-3 content-center items-center">
       <div className="col-start-1 col-end-1 hidden md:block">
+        {router.pathname !== "/" && (
+          <button
+            type="button"
+            className="w-min mr-3"
+            onClick={() => router.back()}
+          >
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </button>
+        )}
         <Link href="/">SCHWEB</Link>
       </div>
       <div className="col-start-1 md:col-start-2 col-end-2">
