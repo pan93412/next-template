b1c912d741dde5dea5351b3626ec879a7edf0fed
Yi-Jyun Pan <pan93412@gmail.com>
2021-05-21 09:56:57 +0800

    feat(tailwind/config): update fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        "sans": "'Noto Sans TC', sans-serif",
+        "serif": "'Noto Serif TC', serif",
+        "mono": "'JetBrains Mono', monospace",
+      },
     },
   },
   variants: {
