8c5e7594b56d6c96df0e00f8865afe6545ff776a
Yi-Jyun Pan <pan93412@gmail.com>
2021-05-23 01:34:33 +0800

    feat(pages/_app): add NProgress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,30 @@
+// Due to NProgress, ESlint always complained these two errors
+// even I prepared the typing file for NProgress. I choose to
+// disable these two and I wish it can be fixed in future.
+// : pan93412 - Mar 23, 2021.
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import React from "react";
+import Router from "next/router";
+import NProgress from "nprogress";
 import type { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
 import "@fontsource/noto-sans-tc";
 import "@fontsource/noto-serif-tc";
 import "jetbrains-mono-webfont";
+import "../styles/nprogress.css";
+
+Router.events.on("routeChangeStart", () => {
+  NProgress.start();
+});
+
+Router.events.on("routeChangeComplete", () => {
+  NProgress.done();
+});
+
+Router.events.on("routeChangeError", () => {
+  NProgress.done();
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   // eslint-disable-next-line react/jsx-props-no-spreading
