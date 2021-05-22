6a437bd7b6afc4035abb17ad0863e309dfac3855
Yi-Jyun Pan <pan93412@gmail.com>
2021-05-23 00:48:28 +0800

    feat(components/Page/BasePage): add <title>

--- components/Page/BasePage.tsx
+++ components/Page/BasePage.tsx
@@ -1,17 +1,24 @@
 import React from "react";
+import Head from "next/head";
 import Navbar from "../Navbar/Navbar";
 import type { ReactNode } from "react";
 
 export interface BasePageProps {
   id: string;
   children: ReactNode;
+  title: string;
 }
 
-export default function BasePage({ id, children }: BasePageProps) {
+export default function BasePage({ title, id, children }: BasePageProps) {
   return (
-    <div className={`page-${id}`}>
-      <Navbar />
-      <div className="p-8">{children}</div>
-    </div>
+    <>
+      <Head>
+        <title>SCHWEB - {title}</title>
+      </Head>
+      <div className={`page-${id}`}>
+        <Navbar />
+        <div className="p-8">{children}</div>
+      </div>
+    </>
   );
 }
