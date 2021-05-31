cb6e387fe619ca488fdb5ab2d86d6014a30b24b2
Yi-Jyun Pan <pan93412@gmail.com>
2021-05-31 08:40:12 +0800

    feat(pages/_app): update to use global.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
 // Due to NProgress, ESlint always complained these two errors
 // even I prepared the typing file for NProgress. I choose to
 // disable these two and I wish it can be fixed in future.
-// : pan93412 - Mar 23, 2021.
+// : pan93412 - May 23, 2021.
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import React from "react";
 import Router from "next/router";
 import NProgress from "nprogress";
 import type { AppProps } from "next/app";
-import "tailwindcss/tailwind.css";
 import "@fontsource/noto-sans-tc";
+import "../styles/global.css";
 import "../styles/nprogress.css";
 
 Router.events.on("routeChangeStart", () => {
