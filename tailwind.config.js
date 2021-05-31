bb13958bcf5190a10bb492f9c602248e323c8ac1
Yi-Jyun Pan <pan93412@gmail.com>
2021-05-31 11:41:05 +0800

    fix(tailwind): explicit safeList

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
     ],
     options: {
-      safelist: ['grid-cols-1', 'grid-cols-4'],
+      safelist: ['grid-cols-1', 'md:grid-cols-2', 'md:grid-cols-3', 'md:grid-cols-4'],
     },
   },
   darkMode: false, // or 'media' or 'class'
