6440923c515d41c7c6e3777a3f13c9ba5e53aaaf
Yi-Jyun Pan <pan93412@gmail.com>
2021-05-31 12:01:18 +0800

    fix(components/Page/ArticlePageComponent): RWD

--- components/Page/ArticlePageComponent.tsx
+++ components/Page/ArticlePageComponent.tsx
@@ -16,14 +16,14 @@ export function ArticlePageComponent({
 }: ArticlePageProps) {
   return (
     <div className="flex flex-col w-full align-left items-left items-center space-y-5">
-      <div className="article w-3/5 meta bg-gray-50 p-5 rounded space-y-1 shadow">
+      <div className="article md:w-3/5 meta bg-gray-50 p-5 rounded space-y-1 shadow">
         {submittedDate && (
           <p className="article date">{submittedDate.toLocaleString()}</p>
         )}
         <p className="article subtitle font-light text-md">{subtitle}</p>
         <h2 className="article title font-bold text-3xl">{title}</h2>
       </div>
-      <div className="article w-3/5 content text-md p-5 leading-relaxed">
+      <div className="article md:w-3/5 content text-md p-5 leading-relaxed">
         {children}
       </div>
     </div>
