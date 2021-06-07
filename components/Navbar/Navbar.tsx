c179ab67a1eba896f3c84b98d8402d97f844ac4a
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-07 08:50:14 +0800

    feat(components/Navbar/Navbar): use <Username>

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,18 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Image from "next/image";
-import Link from "next/link";
-import { displayNameId } from "../LocalDB/consts";
-import LocalDB from "../LocalDB";
-
-const localDB = LocalDB.getInstance();
+import Username from "../DynamicContent/Username";
+import { generateUsername } from "../DynamicContent/Username/utils";
 
 export default function Navbar() {
-  const [user, setUser] = useState("a user");
-  useEffect(() => {
-    const v = localDB.get(displayNameId);
-    if (v) setUser(v);
-  });
-
   return (
     <div className="p-8 grid grid-col-1 md:grid-col-3 content-center items-center">
       <div className="col-start-1 col-end-1">
@@ -28,7 +19,16 @@ export default function Navbar() {
         <Image src="/icon.svg" width="36em" height="36em" />
       </div>
       <div className="col-start-3 col-end-3 justify-self-end">
-        Hi, {user}. <Link href="/setup">[Setup]</Link>
+        Hi, <Username />.{" "}
+        <button
+          type="button"
+          onClick={() => {
+            generateUsername();
+            window.location.reload();
+          }}
+        >
+          [Reset your identity]
+        </button>
       </div>
     </div>
   );
