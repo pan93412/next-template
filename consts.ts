45feb2285909dfe93d4a54839c4f69abe97d56b9
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-27 03:21:17 +0800

    feat(consts): add hint to ENABLE_GA

--- consts.ts
+++ consts.ts
@@ -3,4 +3,9 @@
  * CommonDB and somewhere.
  */
 export const PRODUCT_NAME = "next-template";
+/**
+ * Enable Google Analytics.
+ *
+ * You MUST specify `NEXT_PUBLIC_GA_ID` in your `.env.local` file.
+ */
 export const ENABLE_GA = false;
