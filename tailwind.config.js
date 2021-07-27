55c6b1e82c4dbe09f6f7e7f7f12ffdbaf02f911c
Yi-Jyun Pan <pan93412@gmail.com>
2021-07-28 01:42:26 +0800

    feat(config/tailwind): purge, jit and font tweak

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
+  mode: 'jit',
   darkMode: false, // or 'media' or 'class'
+  purge: [
+    './components/**/*.ts',
+    './components/**/*.tsx',
+    './pages/**/*.tsx',
+    './utilities/**/*.ts',
+    './utilities/**/*.tsx',
+  ],
   theme: {
     extend: {
       fontFamily: {
         "sans": "'PT Sans', 'Noto Sans TC', sans-serif",
         "serif": "'Noto Serif TC', serif",
-        "mono": "'JetBrains Mono', monospace",
+        "mono": "'JetBrains Mono', Menlo, monospace",
       },
     },
   },
